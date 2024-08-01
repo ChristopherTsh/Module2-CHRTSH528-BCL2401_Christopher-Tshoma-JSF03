084eb91d3989f26607d7823cadddaf39e009a516
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-08-01 16:43:41 +0200

    "Added flowbite dependency and imported FwbDropdown component in App.vue, updated package-lock.json and package.json accordingly"

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import {ref, onMounted }from 'vue'
 import ProductCard from './components/ProductCard.vue';
-// import Button from './components/Button.vue';
+import { FwbDropdown} from 'flowbite-vue'
 import axios from "axios"
 
 
@@ -31,7 +31,26 @@ return{
 };
 </script>
 
+
+
 <template>
+   <fwb-dropdown placement="right" text="Right">
+    <div class="w-52">
+      <p class="p-2">
+        Dropdown content line one
+      </p>
+      <p class="p-2">
+        Dropdown content line two
+      </p>
+      <p class="p-2">
+        Dropdown content line three
+      </p>
+      <p class="p-2">
+        Dropdown content line four
+      </p>
+    </div>
+  </fwb-dropdown>
+
   <div class="container mx-auto p-4">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <ProductCard
