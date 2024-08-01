8621d4d93be80026c169f2ed4d6bd7b02bf73760
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-08-01 16:25:16 +0200

    "Updated ProductCard.vue component: refactored props, changed image width, replaced button with Button component, and rearranged card content."

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,47 +2,50 @@
 import Button from "./Button.vue";
 
 const props = defineProps({
-      darkCard: {
-      type: Boolean,
-      default: false,
-    },
-    cardImage: {
-      type: String,
-      default:  "",
-    },
-    cardTitle: {
-      type: String,
-      default: "",
-    },
-    cardDescription: {
-      type: String,
-      default: "",
-    },
-    cardPrice: {
-      type: Number,
-      default:"",
-    },
-    cardCategory: {
-      type: String,
-      default:"",
-    },
+  darkCard: {
+    type: Boolean,
+    default: false,
+  },
+  cardImage: {
+    type: String,
+    default: "",
+  },
+  cardTitle: {
+    type: String,
+    default: "",
+  },
+  cardDescription: {
+    type: String,
+    default: "",
+  },
+  cardPrice: {
+    type: Number,
+    default: "",
+  },
+  cardCategory: {
+    type: String,
+    default: "",
+  },
 });
 </script>
 
 <template>
-    <div :class="['max-w-sm rounded overflow-hidden shadow-lg p-6', darkCard ? 'bg-gray-800 text-white' : 'bg-white text-gray-800']">
-      <img class="w-full" :src="cardImage" alt="Product Image">
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">{{ cardTitle }}</div>
-        <p class="text-base">
-          {{ cardDescription }}
-        </p>
-        <p class="text-base font-semibold mt-2">${{ cardPrice }}</p>
-      </div>
-      <div class="px-6 pt-4 pb-2">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          View Product
-        </button>
-      </div>
+  <div
+    :class="[
+      'max-w-sm rounded overflow-hidden shadow-lg p-6',
+      darkCard ? 'bg-gray-800 text-white' : 'bg-white text-gray-800',
+    ]"
+  >
+    <img class="w-2/5" :src="cardImage" alt="Product Image" />
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ cardTitle }}</div>
+      <p class="text-base">
+        {{ cardCategory }}
+      </p>
+      <p class="text-base font-semibold mt-2">${{ cardPrice }}</p>
     </div>
-  </template>
+    <div class="px-6 pt-4 pb-2">
+      <Button intent="view product">View Product</Button>
+    </div>
+  </div>
+</template>
