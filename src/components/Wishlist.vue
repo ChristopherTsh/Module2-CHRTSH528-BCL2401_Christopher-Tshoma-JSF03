737b88c2814e358a48a13733b40a8245439c7aa8
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-08-06 13:12:06 +0200

    "Refactored Cart and Wishlist components with updated HTML structure and styles"

--- src/components/Wishlist.vue
+++ src/components/Wishlist.vue
@@ -1,39 +1,45 @@
 <template>
-    <h1 class="text-2xl font-bold mb-4 mt-4 flex flex-col items-center">Wishlist</h1>
-     <div class="flex justify-center">
-      
-      <div v-if="wishlist.length === 0" class="text-gray-500">Your wishlist is empty.</div>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" v-else>
-        <div v-for="product in wishlist" :key="product.id" class="mb-4 p-4 border rounded shadow-sm w-80">
-          <p class="text-lg font-semibold">{{ product.title }}</p>
-          <p class="text-gray-700">{{ product.price }}</p>
-          <button 
-            @click="removeFromWishlist(product.id)" 
-            class="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
-          >
-            Remove
-          </button>
-        </div>
+  <h1 class="text-2xl font-bold mb-4 mt-4 flex flex-col items-center">
+    Wishlist
+  </h1>
+  <div class="flex justify-center">
+    <div v-if="wishlist.length === 0" class="text-gray-500">
+      Your wishlist is empty.
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4" v-else>
+      <div
+        v-for="product in wishlist"
+        :key="product.id"
+        class="mb-4 p-4 border rounded shadow-sm w-80"
+      >
+        <p class="text-lg font-semibold">{{ product.title }}</p>
+        <p class="text-gray-700">{{ product.price }}</p>
+        <button
+          @click="removeFromWishlist(product.id)"
+          class="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+        >
+          Remove
+        </button>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { useStore } from 'vuex';
-  
-  export default {
-    setup() {
-      const store = useStore();
-  
-      const removeFromWishlist = (productId) => {
-        store.commit('removeFromWishlist', productId);
-      };
-  
-      return {
-        wishlist: store.state.wishlist,
-        removeFromWishlist,
-      };
-    }
-  };
-  </script>
-  
+  </div>
+</template>
+
+<script>
+import { useStore } from "vuex";
+
+export default {
+  setup() {
+    const store = useStore();
+
+    const removeFromWishlist = (productId) => {
+      store.commit("removeFromWishlist", productId);
+    };
+
+    return {
+      wishlist: store.state.wishlist,
+      removeFromWishlist,
+    };
+  },
+};
+</script>
